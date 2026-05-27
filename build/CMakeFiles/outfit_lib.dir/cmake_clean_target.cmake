6408a04a5687b8d5ee0c970cfaa5570bf90a20e7
Eminadab Abraham <emi@MacBook-Pro.local>
2026-05-27 13:19:31 -0700

    remove build folder and add to gitignore

--- build/CMakeFiles/outfit_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "liboutfit_lib.a"
-)
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/emi/Desktop/Outfit-Generator
-# Build directory: /Users/emi/Desktop/Outfit-Generator/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(OutfitTests "/Users/emi/Desktop/Outfit-Generator/build/tests")
-set_tests_properties(OutfitTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/emi/Desktop/Outfit-Generator/CMakeLists.txt;30;add_test;/Users/emi/Desktop/Outfit-Generator/CMakeLists.txt;0;")
